73288dab9543603982bbc7af995744f94d59dd0b
KOOJAIN <rnwkdls97@naver.com>
2023-04-03 16:17:28 +0900

    4월 3일

---
+++ C++기초/c++기초/c++기초/4-3.cpp
@@ -0,0 +1,33 @@
+//#include<iostream>
+//
+//int main() {
+//	int file_size = 100;								//변수 선언(;로 종료)		// 변수 초기화
+//	double sales = 9.99;
+//	std::cout << file_size;									// 4번줄에 사이즈 크기를 지정해주지않으면 임이의 숫자를 뱉음
+//	return 0;
+//}
+
+//#include<iostream>
+////문제 a가 2가 될수 있는 식을 제작하라
+//using namespace std;					
+//
+//int main() {											
+//	int a = 1;
+//	int b = 2;
+//	///////////////////답			//해석 a b를 담는 공간이 있다. a가 있는동안 b를 담을 수 없고, b가 있는동안 a를 담을 수 없다. 여기서 빈 공간 temp가 더 있다면. 다음과 같이 동작한다. 
+//	//int temp = a;					//  a를 temp값에 옮기고
+//	//a = b;						// 비어있는 a값 공간에 b를 옮긴다
+//	//b = temp;						// 비어있는 b 공간 값에 temp를 옮긴다. 여기서 temp는 a가 되므로 a= 2가 된다.
+//
+//	std::cout << a;
+//	return 0;
+//}
+
+//#include <iostream>
+//
+//int main() {
+//	cout << "Fahrenheit: ";
+//	int fahrenheit;
+//	cin >> fahrenheit;			// double 을 사용 하는 이유는 부동 소숫점이 될수 있기 때문이다
+//	double celsius
+//}
